52cc04daf12f024d9b867e173fde78271bebd313
Raiyan <raiyan.sharif.1234@gmail.com>
2020-07-09 05:49:22 +0600

    project initialitation

---
+++ FootbalQuiz/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FootbalQuiz
+//
+//  Created by raiyan sharif on 9/7/20.
+//  Copyright © 2020 raiyansharif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+
+
+}
+
